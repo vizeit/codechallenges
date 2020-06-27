01b637627f509b9a95b16712b42caf5873637c6e
vizeit <jigparekh@gmail.com>
2020-06-27 18:25:38 -0400

    advance list sort

---
+++ AdvanceListSort.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+def advanced_sort(lst):
+    """
+    Create a function that takes a list of numbers or strings 
+    and returns a list with the items from the original list 
+    stored into sublists. Items of the same value should be in 
+    the same sublist.
+    Examples
+    advanced_sort([2, 1, 2, 1]) ➞ [[2, 2], [1, 1]]
+
+    advanced_sort([5, 4, 5, 5, 4, 3]) ➞ [[5, 5, 5], [4, 4], [3]]
+
+    advanced_sort(["b", "a", "b", "a", "c"]) ➞ [["b", "b"], ["a", "a"], ["c"]]
+
+    Notes
+    The sublists should be returned in the order of each element's 
+    first appearance in the given list.
+    """
+    d = defaultdict(list)
+    for i in lst:
+        d[i].append(i)
+    return [l for l in d.values()]
+
+if __name__ == "__main__":
+    print(advanced_sort([2, 1, 2, 1]))
+    print(advanced_sort([5, 4, 5, 5, 4, 3]))
+    print(advanced_sort(["b", "a", "b", "a", "c"]))
+        
