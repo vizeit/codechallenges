d257b4f050db33fcd7a01e1a85e229d712e77c2b
vizeit <jigparekh@gmail.com>
2020-07-11 22:33:06 -0400

    optimize

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -1,21 +1,6 @@
 def convertnum(c):
     return int(c) if c.find('.')==-1 else float(c)
 
-def parseexpression(s):
-    rs, i, ts = [], 0, ''
-    for c in s:
-        if c == ' ': continue
-        elif c in('.0123456789'): ts += c
-        elif c in ('+-*/()'):
-            if len(ts): rs.append(convertnum(ts))
-            if len(rs) and isinstance(rs[-1],str) and ((rs[-1] in ('+-*/') and c in('+-*/')) or (rs[-1] == '(' and c == ')')):
-                raise Exception('Invalid expression')
-            rs.append(c)
-            ts = ''
-        else: raise Exception('Invalid expression')
-    if len(ts): rs.append(convertnum(ts))
-    return rs
-
 def operation(c, operator, stack):
     if operator in (' +-'):
         stack.append(c) if operator in ('','+') else stack.append(-c)
@@ -28,12 +13,18 @@ def calculator(s):
     if len(s) == 0: raise Exception('Invalid Expression')
     stack = []
     operator = ''
-    while len(s):
-        c = s.pop(0)
-        if isinstance(c, int) or isinstance(c, float):
-            operation(c, operator, stack)
-        elif c in ('+-*/'):
-            operator = c
+    ts = ''
+    while len(s) >= 0:
+        c = s.pop(0) if len(s) else '-1'
+        if c == ' ': continue
+        elif c in('.0123456789'): ts += c
+        elif c in ('+-*/') or (c == '-1' and len(s) == 0) or c == ')':
+            if (len(ts)==0 and len(stack)==0) or (len(ts)==0 and len(operator) and operator in '+-*/' and c in '+-*/' ): 
+                raise Exception('Invalid Expression')
+            elif len(ts): operation(convertnum(ts), operator, stack)
+            if c == '-1' or c == ')': break
+            else: operator = c
+            ts = ''
         elif c == '(':
             n = calculator(s) 
             operation(n, operator, stack)
@@ -43,11 +34,13 @@ def calculator(s):
 
 if __name__ == "__main__":
     try:
-        print(calculator(parseexpression('1.1*2.2* 3.3')))
-        print(calculator(parseexpression('1+2*3/2')))
-        print(calculator(parseexpression('(1+2)*6')))
-        print(calculator(parseexpression('(2*(1+2))*3')))
-        print(calculator(parseexpression('2*((1+2)*(2+3))*2')))
+        print(calculator(list('1+1')))
+        print(calculator(list('1.1*2.2* 3.3')))
+        print(calculator(list('1+2*3/2')))
+        print(calculator(list('(1+2)*6')))
+        print(calculator(list('(2*(1+2))*3')))
+        print(calculator(list('(2*((1+2)*(2+3)))*2')))
+        print(calculator(list('1*/1')))
     except Exception as e:
         print(e)
 
